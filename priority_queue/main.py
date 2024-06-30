f8ffa87f327b7e3986b63c67e5562318379305e3
saeed <saeedmouzarmi@gmail.com>
2024-06-30 21:11:04 +0330

    Added another python file for priority class

--- priority_queue/main.py
+++ priority_queue/main.py
@@ -1,32 +1,7 @@
-from array import array
-
-
-class PriorityQueue:
-    def __init__(self):
-        self.items = []
-        self.priorities = {}
-
-    def append(self, item, priority=0):
-        self.items.append(item)
-        self.priorities[item] = priority
-
-    def max_item(self, input_array: dict):
-        max_priority = max(input_array.values())
-        key = 0
-        for (k, v) in input_array.items():
-            if v == max_priority:
-                key = k
-                break
-        return key, max_priority
-
-    def pop(self):
-        max_priority = self.max_item(self.priorities)
-        self.priorities.pop(max_priority[0])
-        self.items.remove(max_priority[0])
-        return max_priority[0]
-
+from priority_queue import PriorityQueue
 
 a = PriorityQueue()
+
 a.append("one", 1)
 a.append("two", 2)
 a.append("three", 3)
---
+++ priority_queue/priority_queue.py
@@ -0,0 +1,30 @@
+from array import array
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.items = []
+        self.priorities = {}
+
+    def append(self, item, priority=0):
+        self.items.append(item)
+        self.priorities[item] = priority
+
+    def max_item(self, input_array: dict):
+        max_priority = max(input_array.values())
+        key = 0
+        for (k, v) in input_array.items():
+            if v == max_priority:
+                key = k
+                break
+        return key, max_priority
+
+    def pop(self):
+        max_priority = self.max_item(self.priorities)
+        self.priorities.pop(max_priority[0])
+        self.items.remove(max_priority[0])
+        return max_priority[0]
+
+
+if __name__ == '__main__':
+    pq = PriorityQueue()
